0faf1ae7281d0d6f65bbf9fdc4a6605922e37825
andrius-win-desktop <andrius_46r@gmail.com>
2022-07-13 13:03:52 +0200

    Añadidas clase Task y componentes TaskList y Task

---
+++ src/components/pure/greetingF.jsx
@@ -0,0 +1,38 @@
+import React,  {useState}from 'react'
+import PropTypes from 'prop-types'
+
+const GreetingF = (props) => {
+
+  // Breve introduction a useState  
+  //const [variable, método para actualizarlo] = useState(valor inicial)
+  const [age, setAge] = useState(40)
+  const birthday = () => {
+    // actualizamos el state
+    setAge(age + 1)
+  }
+  
+  return (
+    <div>
+
+        <h1>
+          Hola { props.name } desde componente funcional.
+        </h1>        
+        <h2>
+          Tu edad es de: { age }        
+        </h2>        
+        <div>
+          <button onClick={ birthday }>
+            Cumplir Años
+          </button>
+        </div>
+        
+        
+    </div>
+  )
+}
+
+GreetingF.propTypes = {
+    name: PropTypes.string
+}
+
+export default GreetingF;
